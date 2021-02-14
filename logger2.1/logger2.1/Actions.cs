5f953451ab2af6bf1aad7ef0074a73f288388f30
Tykan-PC\Tykan <tykan4uk2010@yandex.ru>
2021-02-14 16:51:47 +0200

    Add class Actions.

---
+++ logger2.1/logger2.1/Actions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerModule2
+{
+    public class Actions
+    {
+        public Result StartMethod()
+        {
+            Logger.AddLog(CodeLog.Info, $"Start method: {nameof(StartMethod)}");
+            return new Result(true);
+        }
+
+        public Result SkipLogic()
+        {
+            Logger.AddLog(CodeLog.Warning, $"Skipped logic in method: {nameof(SkipLogic)}");
+            return new Result(true);
+        }
+
+        public Result BrokeLogic()
+        {
+            Logger.AddLog(CodeLog.Error, $"Broken logic: {nameof(BrokeLogic)}");
+            return new Result(false, "I broke a logic.");
+        }
+    }
+}
