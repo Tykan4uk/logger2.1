5ab8e841790e747f97f30e1309ce499dfc5836fb
Tykan-PC\Tykan <tykan4uk2010@yandex.ru>
2021-02-14 15:53:45 +0200

    Class Logger.

---
+++ logger2.1/logger2.1/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerModule2
+{
+   public class Logger
+{
+        private static Logger instance;
+        private static StringBuilder _log = new StringBuilder();
+
+        private Logger()
+        {
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+
+        public static void AddLog(CodeLog codelog, string msg)
+        {
+            _log.Append($"{DateTime.Now}: {codelog}: {msg}\n");
+        }
+
+        public static string GetLog()
+        {
+            return _log.ToString();
+        }
+}
+}
